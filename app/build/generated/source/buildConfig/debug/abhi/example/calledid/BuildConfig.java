2f9918d24d0b474c5d03eed4643208be5c17f9a3
abhimangalmsrently <108400983+abhimangalmsrently@users.noreply.github.com>
2023-01-04 16:01:44 +0530

    initial commit

---
+++ app/build/generated/source/buildConfig/debug/abhi/example/calledid/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package abhi.example.calledid;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "abhi.example.calledid";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
